049513dfdcbb63a309aa025462b7a37ff1c10ea3
mohamed Said elbakry <mohamed.elbakry@Devart.Local>
2026-07-27 21:06:54 +0300

    تحديث منصة محفظة الكامل والنهائي

--- sync.bat
+++ sync.bat
@@ -4,12 +4,10 @@ echo 🚀 جاري رفع وتحديث مشروع محفظة على GitHub تل�
 echo ==================================================
 set GIT_EXE="C:\Program Files\Git\cmd\git.exe"
 %GIT_EXE% init
-%GIT_EXE% remote remove origin 2>nul
-%GIT_EXE% remote add origin https://github.com/mo7medelbakry/maahfaztk.git
 %GIT_EXE% branch -M main
 %GIT_EXE% add .
-%GIT_EXE% commit -m "تحديث تلقائي للمنصة من Antigravity"
-%GIT_EXE% push -u origin main
+%GIT_EXE% commit -m "تحديث منصة محفظة"
+%GIT_EXE% push -u origin main --force
 echo ==================================================
 echo 🎉 تم رفع التحديث بنجاح! موقعك على Render سيتحدث آلياً خلال 30 ثانية.
 echo ==================================================
